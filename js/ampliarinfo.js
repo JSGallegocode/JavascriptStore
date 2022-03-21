80984bc40e36426e9212151a392e04712d3b2264
unknown <locomas2020@gmail.com>
2022-03-21 09:15:47 -0500

    final version

---
+++ js/ampliarinfo.js
@@ -0,0 +1,33 @@
+ export function ampliarInformacion(event) {
+
+     let producto = {}
+     if (event.target.classList.contains("btn")) {
+
+
+         producto = {
+
+             foto: event.target.parentElement.querySelector("img").src,
+             nombre: event.target.parentElement.querySelector("h2").textContent,
+             descripcion: event.target.parentElement.querySelector("h4").textContent,
+             precio: event.target.parentElement.querySelector("h6").textContent,
+
+
+         }
+
+         console.log(producto)
+             //asociando valores al modal 
+
+         let foto = document.getElementById("fotoinfo")
+         foto.src = producto.foto
+         let nombre = document.getElementById("nombreinfo")
+         nombre.textContent = producto.nombre
+         let descripcion = document.getElementById("descrip")
+         descripcion.textContent = producto.descripcion
+         let precio = document.getElementById("preciog")
+         precio.textContent = producto.precio
+
+     }
+
+
+
+ }
---
+++ test.js
@@ -0,0 +1,2 @@
+total = 200;
+let descuento = total * 0.5;
